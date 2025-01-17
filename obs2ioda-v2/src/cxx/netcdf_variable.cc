a2a3e393b51fbe7a534a2ce8e444db8e0c10a8bf
amstokely <andystokely@gmail.com>
2025-01-17 11:21:26 -0700

    Added support for adding netcdf dimensions.

---
+++ obs2ioda-v2/src/cxx/netcdf_variable.cc
@@ -0,0 +1,358 @@
+#include "netcdf_variable.h"
+#include "netcdf_utils.h"
+#include "netcdf_file.h"
+#include <algorithm>
+#include <mutex>
+
+namespace Obs2Ioda {
+
+    template int netcdfGetVar<int>(
+            int,
+            const char *,
+            const char *,
+            int **
+    );
+
+    template int netcdfGetVar<long long>(
+            int,
+            const char *,
+            const char *,
+            long long **
+    );
+
+    template int netcdfGetVar<float>(
+            int,
+            const char *,
+            const char *,
+            float **
+    );
+
+    template int netcdfSetFill<int>(
+            int,
+            const char *,
+            const char *,
+            int,
+            int
+    );
+
+    template int netcdfSetFill<float>(
+            int,
+            const char *,
+            const char *,
+            int,
+            float
+    );
+
+    template int netcdfSetFill<long long>(
+            int,
+            const char *,
+            const char *,
+            int,
+            long long
+    );
+
+    template int netcdfSetFill<const char *>(
+            int,
+            const char *,
+            const char *,
+            int,
+            const char *
+    );
+
+    template int netcdfSetFill<const char>(
+            int,
+            const char *,
+            const char *,
+            int,
+            const char
+    );
+
+    template int netcdfPutVar<int>(
+            int,
+            const char *,
+            const char *,
+            const int *
+    );
+
+    template int netcdfPutVar<float>(
+            int,
+            const char *,
+            const char *,
+            const float *
+    );
+
+    int netcdfAddVar(
+            int netcdfID,
+            const char *groupName,
+            const char *varName,
+            nc_type netcdfDataType,
+            int numDims,
+            const char **dimNames
+    ) {
+        try {
+            std::lock_guard<std::mutex> lock(map_mutex);
+            auto group = getRootGroup(
+                    netcdfID,
+                    groupName
+            );
+            auto file = NETCDF_FILE_MAP[netcdfID];
+            std::vector<netCDF::NcDim> dims;
+            dims.reserve(numDims);
+            for (int i = 0; i < numDims; i++) {
+                dims.push_back(file->getDim(dimNames[i]));;
+            }
+            auto var = group->addVar(
+                    varName,
+                    netCDF::NcType(netcdfDataType),
+                    dims
+            );
+            return 0;
+        } catch (netCDF::exceptions::NcException &e) {
+            return netcdfErrorMessage(
+                    e,
+                    1
+            );
+        }
+    }
+
+    template<typename T>
+    int netcdfPutVar(
+            int netcdfID,
+            const char *groupName,
+            const char *varName,
+            const T *data
+    ) {
+        try {
+            std::lock_guard<std::mutex> lock(map_mutex);
+            auto file = NETCDF_FILE_MAP[netcdfID];
+            std::shared_ptr<netCDF::NcGroup> group = getRootGroup(
+                    netcdfID,
+                    groupName
+            );
+            auto var = group->getVar(varName);
+            var.putVar(data);
+            return 0;
+        } catch (netCDF::exceptions::NcException &e) {
+            return netcdfErrorMessage(
+                    e,
+                    1
+            );
+        }
+    }
+
+    int netcdfPutVarInt(
+            int netcdfID,
+            const char *groupName,
+            const char *varName,
+            const int *data
+    ) {
+        return netcdfPutVar(
+                netcdfID,
+                groupName,
+                varName,
+                data
+        );
+    }
+
+    int netcdfPutVarInt64(
+            int netcdfID,
+            const char *groupName,
+            const char *varName,
+            const long long *data
+    ) {
+        return netcdfPutVar(
+                netcdfID,
+                groupName,
+                varName,
+                data
+        );
+    }
+
+    int netcdfPutVarReal(
+            int netcdfID,
+            const char *groupName,
+            const char *varName,
+            const float *data
+    ) {
+        return netcdfPutVar(
+                netcdfID,
+                groupName,
+                varName,
+                data
+        );
+    }
+
+    int netcdfPutVarString(
+            int netcdfID,
+            const char *groupName,
+            const char *varName,
+            const char **data
+    ) {
+        return netcdfPutVar(
+                netcdfID,
+                groupName,
+                varName,
+                data
+        );
+    }
+
+
+    template<typename T>
+    int netcdfGetVar(
+            int netcdfID,
+            const char *groupName,
+            const char *varName,
+            T **data
+    ) {
+        try {
+            std::lock_guard<std::mutex> lock(map_mutex);
+            auto file = NETCDF_FILE_MAP[netcdfID];
+            auto group = getRootGroup(
+                    netcdfID,
+                    groupName
+            );
+            auto var = group->getVar(varName);
+            const std::vector<size_t> start = {0};
+            const std::vector<size_t> count = {var.getDim(0).getSize()};
+            var.getVar(
+                    start,
+                    count,
+                    *data
+            );
+            return 0;
+        } catch (netCDF::exceptions::NcException &e) {
+            return netcdfErrorMessage(
+                    e,
+                    1
+            );
+        }
+    }
+
+    template<typename T>
+    int netcdfSetFill(
+            int netcdfID,
+            const char *groupName,
+            const char *varName,
+            int fillMode,
+            T fillValue
+    ) {
+        try {
+            std::lock_guard<std::mutex> lock(map_mutex);
+            auto file = NETCDF_FILE_MAP[netcdfID];
+            std::shared_ptr<netCDF::NcGroup> group = getRootGroup(
+                    netcdfID,
+                    groupName
+            );
+            auto var = group->getVar(varName);
+            var.setFill(
+                    fillMode,
+                    fillValue
+            );
+            return 0;
+        } catch (netCDF::exceptions::NcException &e) {
+            return netcdfErrorMessage(
+                    e,
+                    1
+            );
+        }
+    }
+
+    int netcdfSetFillInt(
+            int netcdfID,
+            const char *groupName,
+            const char *varName,
+            int fillMode,
+            int fillValue
+    ) {
+        return netcdfSetFill(
+                netcdfID,
+                groupName,
+                varName,
+                fillMode,
+                fillValue
+        );
+    }
+
+    int netcdfSetFillReal(
+            int netcdfID,
+            const char *groupName,
+            const char *varName,
+            int fillMode,
+            float fillValue
+    ) {
+        return netcdfSetFill(
+                netcdfID,
+                groupName,
+                varName,
+                fillMode,
+                fillValue
+        );
+    }
+
+    int netcdfSetFillInt64(
+            int netcdfID,
+            const char *groupName,
+            const char *varName,
+            int fillMode,
+            long long fillValue
+    ) {
+        return netcdfSetFill(
+                netcdfID,
+                groupName,
+                varName,
+                fillMode,
+                fillValue
+        );
+    }
+
+    int netcdfSetFillString(
+            int netcdfID,
+            const char *groupName,
+            const char *varName,
+            int fillMode,
+            const char *fillValue
+    ) {
+        return netcdfSetFill(
+                netcdfID,
+                groupName,
+                varName,
+                fillMode,
+                fillValue
+
+        );
+    }
+
+    int netcdfGetVarString1D(
+            int netcdfID,
+            const char *groupName,
+            const char *varName,
+            char ***data
+    ) {
+        auto file = NETCDF_FILE_MAP[netcdfID];
+        auto dims = file->getVar(varName).getDims();
+        auto var = file->getVar(varName);
+        size_t numStrings = dims[0].getSize();
+        char **buffer = new char *[numStrings];
+        int retval = netcdfGetVar(
+                netcdfID,
+                groupName,
+                varName,
+                &buffer
+        );
+        for (auto i = 0; i < numStrings; i++) {
+            std::string tmpStr = buffer[i];
+            std::copy(
+                    tmpStr.begin(),
+                    tmpStr.end(),
+                    (*data)[i]
+            );
+            (*data)[i][tmpStr.size()] = '\0';
+        }
+        var.freeString(
+                numStrings,
+                (char **) buffer
+        );
+        free(buffer);
+        return retval;
+    }
+}
--- obs2ioda-v2/src/netcdf_cxx_i_mod.f90
+++ obs2ioda-v2/src/netcdf_cxx_i_mod.f90
@@ -95,6 +95,69 @@ module netcdf_cxx_i_mod
             integer(c_int) :: c_netcdfAddGroup
         end function c_netcdfAddGroup
 
+        function c_netcdfAddVar(&
+                netcdfID, groupName, varName, netcdfDataType, numDims, dimNames) &
+                bind(C, name = "netcdfAddVar")
+            import :: c_int
+            import :: c_ptr
+            integer(c_int), value, intent(in) :: netcdfID
+            type(c_ptr), value, intent(in) :: groupName
+            type(c_ptr), value, intent(in) :: varName
+            integer(c_int), value, intent(in) :: netcdfDataType
+            integer(c_int), value, intent(in) :: numDims
+            type(c_ptr), value, intent(in) :: dimNames
+            integer(c_int) :: c_netcdfAddVar
+        end function c_netcdfAddVar
+
+        function c_netcdfPutVarInt(&
+                netcdfID, groupName, varName, data) &
+                bind(C, name = "netcdfPutVarInt")
+            import :: c_int
+            import :: c_ptr
+            integer(c_int), value, intent(in) :: netcdfID
+            type(c_ptr), value, intent(in) :: groupName
+            type(c_ptr), value, intent(in) :: varName
+            type(c_ptr), value, intent(in) :: data
+            integer(c_int) :: c_netcdfPutVarInt
+        end function c_netcdfPutVarInt
+
+        function c_netcdfPutVarInt64(&
+                netcdfID, groupName, varName, data) &
+                bind(C, name = "netcdfPutVarInt64")
+            import :: c_int
+            import :: c_ptr
+            integer(c_int), value, intent(in) :: netcdfID
+            type(c_ptr), value, intent(in) :: groupName
+            type(c_ptr), value, intent(in) :: varName
+            type(c_ptr), value, intent(in) :: data
+            integer(c_int) :: c_netcdfPutVarInt64
+        end function c_netcdfPutVarInt64
+
+
+        function c_netcdfPutVarReal(&
+                netcdfID, groupName, varName, data) &
+                bind(C, name = "netcdfPutVarReal")
+            import :: c_int
+            import :: c_ptr
+            integer(c_int), value, intent(in) :: netcdfID
+            type(c_ptr), value, intent(in) :: groupName
+            type(c_ptr), value, intent(in) :: varName
+            type(c_ptr), value, intent(in) :: data
+            integer(c_int) :: c_netcdfPutVarReal
+        end function c_netcdfPutVarReal
+
+        function c_netcdfPutVarString(&
+                netcdfID, groupName, varName, data) &
+                bind(C, name = "netcdfPutVarString")
+            import :: c_int
+            import :: c_ptr
+            integer(c_int), value, intent(in) :: netcdfID
+            type(c_ptr), value, intent(in) :: groupName
+            type(c_ptr), value, intent(in) :: varName
+            type(c_ptr), value, intent(in) :: data
+            integer(c_int) :: c_netcdfPutVarString
+        end function c_netcdfPutVarString
+
     end interface
 
 end module netcdf_cxx_i_mod
