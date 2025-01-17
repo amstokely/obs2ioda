8e65629b514297a4ca44de044b4200ad7a5ab988
amstokely <andystokely@gmail.com>
2025-01-16 19:41:47 -0700

    Deleted build dir

--- build/obs2ioda-v2/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andy/CLionProjects/obs2ioda/obs2ioda-v2
-# Build directory: /home/andy/CLionProjects/obs2ioda/build/obs2ioda-v2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/cxx")
--- build/obs2ioda-v2/src/cxx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andy/CLionProjects/obs2ioda/obs2ioda-v2/src/cxx
-# Build directory: /home/andy/CLionProjects/obs2ioda/build/obs2ioda-v2/src/cxx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
